0c3416679c7b9507f76671caba3738a52f07b7a4
“Anshul <anshulr2010@gmail.com>
2025-05-20 20:20:13 +0530

    Visualize connectivity in network

---
+++ Assignment_3/visualize_connectivity.py
@@ -0,0 +1,158 @@
+# Imports for visualising the model connectivity
+from sklearn.preprocessing import MinMaxScaler
+from keras.callbacks import ModelCheckpoint
+
+from IPython.display import HTML as html_print
+from IPython.display import display
+import tensorflow.keras.backend as K
+import tensorflow as tf
+import numpy as np
+
+# Tools for getting model connectivity between input and output characters
+def get_lstm_output(decoder, x, hidden, enc_out=None):
+    
+    x = decoder.embedding_layer(x)
+
+    if decoder.attention:
+        context_vector, attention_weights = decoder.attention_layer(hidden, enc_out)
+        x = tf.concat([tf.expand_dims(context_vector, 1), x], -1)
+    else:
+        attention_weights = None
+
+    if decoder.layer_type == "lstm":
+        output, h_state, c_state = decoder.rnn_layers[0](x, initial_state=hidden)
+        state = [h_state, c_state]
+    else:
+        output, state = decoder.rnn_layers[0](x, initial_state=hidden)
+
+    for layer in decoder.rnn_layers[1:]:
+        if decoder.layer_type == "lstm":
+            output, _, _ = layer(output)
+        else:
+            output, _ = layer(output)
+    
+    return output, state, attention_weights
+
+def get_output_from_embedding(encoder, x, hidden):
+
+    if encoder.layer_type == "lstm":
+        output, h_state, c_state = encoder.rnn_layers[0](x, initial_state=hidden)
+        state = [h_state, c_state]
+    else:
+        output, state = encoder.rnn_layers[0](x, initial_state=hidden)
+
+    for layer in encoder.rnn_layers[1:]:
+        if encoder.layer_type == "lstm":
+            output, _, _ = layer(output)
+        else:
+            output, _ = layer(output)
+
+    return output, state
+
+
+def get_connectivity(model, word):
+
+    word = "\t" + word + "\n"
+
+    inputs = model.input_tokenizer.texts_to_sequences([word])
+    inputs = tf.keras.preprocessing.sequence.pad_sequences(inputs,
+                                                            maxlen=model.max_input_len,
+                                                            padding="post")
+
+    result = ""
+
+    gradient_list = []
+
+    enc_state = model.encoder.initialize_hidden_state(1)
+    embedded_in = model.encoder.embedding(inputs)
+
+
+    with tf.GradientTape(persistent=True, watch_accessed_variables=False) as tape:
+        tape.watch(embedded_in)
+
+        enc_out, enc_state = get_output_from_embedding(model.encoder, embedded_in, enc_state)
+
+        dec_state = enc_state
+        dec_input = tf.expand_dims([model.targ_tokenizer.word_index["\t"]]*1, 1)
+
+        for t in range(1, model.max_target_len):
+
+            lstm_out, dec_state, _ = get_lstm_output(model.decoder, dec_input, dec_state, enc_out)
+
+            preds = model.decoder.dense(model.decoder.flatten(lstm_out))
+            gradient_list.append(tape.gradient(lstm_out, embedded_in)[0])
+            
+            preds = tf.argmax(preds, 1)
+            next_char = model.targ_tokenizer.index_word[preds.numpy().item()]
+            result += next_char
+
+            dec_input = tf.expand_dims(preds, 1)
+
+            if next_char == "\n":
+                return result[:-1], gradient_list[:-1]
+
+        return result[:-1], gradient_list[:-1]
+    
+# get html element
+def cstr(s, color='black'):
+    if s == ' ':
+      return "<text style=color:#000;padding-left:10px;background-color:{}> </text>".format(color, s)
+    else:
+      return "<text style=color:#000;background-color:{}>{} </text>".format(color, s)
+	
+# print html
+def print_color(t):
+	  display(html_print(''.join([cstr(ti, color=ci) for ti,ci in t])))
+
+# get appropriate color for value
+def get_clr(value):
+    colors = ['#85c2e1', '#89c4e2', '#95cae5', '#99cce6', '#a1d0e8'
+      '#b2d9ec', '#baddee', '#c2e1f0', '#eff7fb', '#f9e8e8',
+      '#f9e8e8', '#f9d4d4', '#f9bdbd', '#f8a8a8', '#f68f8f',
+      '#f47676', '#f45f5f', '#f34343', '#f33b3b', '#f42e2e']
+    value = int(value * 19)
+    if value == 19:
+        value -= 1
+    return colors[value]
+
+# sigmoid function
+def sigmoid(x):
+    z = 1/(1 + np.exp(-x)) 
+    return z
+
+def softmax(x):
+    v = np.exp(x)
+    v = v / np.sum(v)
+    return v
+
+def get_gradient_norms(grad_list, word, activation="sigmoid"):
+    grad_norms = []
+    for grad_tensor in grad_list:
+        grad_mags = tf.norm(grad_tensor, axis=1)
+        grad_mags = grad_mags[:len(word)]
+        if activation == "softmax":
+            grad_mags_scaled = softmax(grad_mags)
+        elif activation == "scaler":
+            scaler = MinMaxScaler()
+            grad_mags = tf.reshape(grad_mags, (-1,1))
+            grad_mags_scaled = scaler.fit_transform(grad_mags)
+        else:
+            grad_mags_scaled = sigmoid(grad_mags)
+        grad_norms.append(grad_mags_scaled)
+    return grad_norms
+
+def visualize(grad_norms, word, translated_word):
+    print("Original Word:", word)
+    print("Transliterated Word:", translated_word)
+    for i in range(len(translated_word)):
+        print("Connectivity Visualization for", translated_word[i],":")
+        text_colours = []
+        for j in range(len(grad_norms[i])):
+            text = (word[j], get_clr(grad_norms[i][j]))
+            text_colours.append(text)
+        print_color(text_colours)
+
+def visualise_connectivity(model, word, activation="sigmoid"):
+    translated_word, grad_list = get_connectivity(model, word)
+    grad_norms = get_gradient_norms(grad_list, word, activation)
+    visualize(grad_norms, word, translated_word)
